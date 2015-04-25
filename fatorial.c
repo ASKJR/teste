ee3abb8aecbaad05b446cf5541326242d03086d5
ASKJR <albertokatojr@gmail.com>
2015-04-25 16:15:35 -0300

    Fatorial em C
    - int fatorial (int n).
    - main para testar a função fatorial.

---
+++ fatorial.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int fatorial (int n){
+	int i,fat=1;
+	for(i=n;i>=1;i--){
+		   	fat*=i;
+	}
+	return fat;
+}
+
+int main(){
+	int num;
+	printf("Digite um numero > ");
+	scanf("%d",&num);
+	printf("O fatorial do numero digitado eh: %d\n",fatorial(num));
+	return 0;
+}
